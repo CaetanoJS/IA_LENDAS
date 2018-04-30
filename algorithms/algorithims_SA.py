364d5e0502cfabe3b3428311d68cbe0cf5a36ba5
Caetanojs <caetanoj.ss@gmail.com>
2018-04-30 12:59:10 -0300

    Simulated pronto, necessita apenas testar na função learn

--- algorithms/algorithims_SA.py
+++ algorithms/algorithims_SA.py
@@ -1,23 +1,52 @@
+# -*- coding: utf-8 -*-
+import math
+import random
 #######################Simulated Annealing#####################################
 
     #funcao de aprendizado do SA
-    def simulated_annealing(self, weights):
-        #define estado inicial como um estado aleatorio
-        estado_atual = selecionaEstadoAleatorio()
-        for t in inf():
-            T = decresce_temperatura(t)
-            if( T == min_temperatura):
-	        return estado_atual
-            for i in range(1,max_vizinhos):
-                estado_candidato = seleciona_vizinho_aleatorio(estado_atual)
-	        variacao_E = self.run_episode(estado_candidato) - self.run_episode(estado_atual)
-                if (variacao_E > 0):
+def simulated_annealing(weights):
+    #define estado inicial como um estado aleatorio
+    estado_atual = weights
+    min_temperatura = 1
+    T = 100
+    while 1==1:
+        T = T - 0.2
+        if( T == min_temperatura):
+            return estado_atual
+        for i in range(1,10):
+            estado_candidato = geraVizinhos(estado_atual)
+            variacao_E = self.run_episode(estado_candidato) - self.run_episode(estado_atual)
+            if (variacao_E > 0):
+                print("estado candidato é melhor que estado atual")
+                estado_atual = estado_candidato
+            else:
+                value = math.exp(-variacao_E/T)
+                verifica_prob = random.uniform(0,1)
+                if(verifica_prob < value):
+                    print("estado candidato é pior  que estado atual mas é escolhido pelo algoritimo de prob")
                     estado_atual = estado_candidato
-                else:
-		    estado_atual = probabilidade_estado_candidato()#função que faz a probabilidade de um valor ser aceito
 
-	def probabilidade_estado_candidato(self)
-	    self.value = exp(-variação_E/T)
-		return self.value
+
+
+
+def probabilidade_estado_candidato():
+    value = math.exp(-variacao_E/T)
+    return value
+
+
+def geraVizinhos(estado):
+    perturbacao = 0.5
+    vizinhos = []
+
+
+    vi = estado[:]
+    perturbacao = random.uniform(-2,2)
+
+    for j in range(len(vi)):
+        vi[j] += perturbacao
+
+    return vi
+
+
 
 #################################################################################
