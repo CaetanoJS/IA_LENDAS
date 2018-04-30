d8426618708555a7e6defd3768a127427177f6f3
CaetanoJS <caetanoj.ss@gmail.com>
2018-04-30 18:21:12 -0300

    Algoritimo SA funcionando

--- algorithms/algorithims_SA.py
+++ algorithms/algorithims_SA.py
@@ -1,52 +1,53 @@
-# -*- coding: utf-8 -*-
-import math
-import random
-#######################Simulated Annealing#####################################
+    def Simulated_Annealing(self, weights):
 
-    #funcao de aprendizado do SA
-def simulated_annealing(weights):
-    #define estado inicial como um estado aleatorio
-    estado_atual = weights
-    min_temperatura = 1
-    T = 100
-    while 1==1:
-        T = T - 0.2
-        if( T == min_temperatura):
-            return estado_atual
-        for i in range(1,10):
-            estado_candidato = geraVizinhos(estado_atual)
-            variacao_E = self.run_episode(estado_candidato) - self.run_episode(estado_atual)
-            if (variacao_E > 0):
-                print("estado candidato é melhor que estado atual")
-                estado_atual = estado_candidato
-            else:
-                value = math.exp(-variacao_E/T)
-                verifica_prob = random.uniform(0,1)
-                if(verifica_prob < value):
-                    print("estado candidato é pior  que estado atual mas é escolhido pelo algoritimo de prob")
+
+            #gera vizinhos de um estado
+        def geraVizinhos(estado):
+            perturbacao = 0.5
+            vizinhos = []
+
+
+            vi = estado[:]
+
+
+            for j in range(len(vi)):
+                perturbacao = random.uniform(-1,1)
+                vi[j] += perturbacao
+
+            return vi
+
+        estado_atual = [5,2,1,3,2,1]
+        min_temperatura = 0
+        T = 200
+        while 1==1:
+
+            T = T - 1
+            print (T)
+            if( T == min_temperatura):
+                return estado_atual
+
+
+            for i in range(1,20):
+                estado_candidato = geraVizinhos(estado_atual)
+                candidato = self.run_episode(estado_candidato)
+                atual = self.run_episode(estado_atual)
+                variacao_E =  candidato - atual
+
+                print("Variação de E :",variacao_E)
+
+                if (variacao_E > 0):
+                    print("Variação positiva")
                     estado_atual = estado_candidato
 
+                elif(variacao_E < 0):
+                    value = math.exp(variacao_E/T)
+                    verifica_prob = random.uniform(0,1)
 
+                    print("variação negativa")
+                    print("verifica prob:", verifica_prob)
+                    print('Função de prob: %.10f' % value)
 
+                    if(verifica_prob < value):
+                        print("Acertou probabilidade")
+                        estado_atual = estado_candidato
 
-def probabilidade_estado_candidato():
-    value = math.exp(-variacao_E/T)
-    return value
-
-
-def geraVizinhos(estado):
-    perturbacao = 0.5
-    vizinhos = []
-
-
-    vi = estado[:]
-    perturbacao = random.uniform(-2,2)
-
-    for j in range(len(vi)):
-        vi[j] += perturbacao
-
-    return vi
-
-
-
-#################################################################################
