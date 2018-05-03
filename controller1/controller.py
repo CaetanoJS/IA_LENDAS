52f2ac3100c317069b82fe9e824f085608665081
CaetanoJS <caetanoj.ss@gmail.com>
2018-05-03 00:20:02 -0300

    mergin

--- controller1/controller.py
+++ controller1/controller.py
@@ -1,5 +1,6 @@
 import controller_template as controller_template
 import random
+import math
 
 
 class Controller(controller_template.Controller):
@@ -56,32 +57,33 @@ class Controller(controller_template.Controller):
         oxygen = self.normaliza_feature(sensors[12], 1, 400)
 
         features_list = []
-        a = (1/(obstacle_AHEAD + 0.1)) #max 10 min 0.9090909091
-        b = (1/(obstacle_DOWN + 0.1))  #max 10 min 0.9090909091
-        c = (1/(obstacle_DOWN_RIGHT + 0.1))  #max 10 min 0.9090909091
-        d = (1/(monster_AHEAD + 0.1)) #max 10 min 0.9090909091
-        e = (1/(monster_DOWN + 0.1))
-        f = (1/(monster_DOWN_RIGHT + 0.1))
-        g = (1/(obstacle_UP + 0.1))
-        h = (1/(obstacle_UP_RIGHT + 0.1))
-        i = (1/(monster_UP + 0.1))
-        j = (1/(monster_UP_RIGHT + 0.1))
-        k = (1/(water_UP + 0.1))
-        l = (1/(water_UP_RIGHT + 0.1))
+        # a = (1/(obstacle_AHEAD + 0.1)) #max 10 min 0.9090909091
+        # b = (1/(obstacle_DOWN + 0.1))  #max 10 min 0.9090909091
+        # c = (1/(obstacle_DOWN_RIGHT + 0.1))  #max 10 min 0.9090909091
+        # d = (1/(monster_AHEAD + 0.1)) #max 10 min 0.9090909091
+        # e = (1/(monster_DOWN + 0.1))
+        # f = (1/(monster_DOWN_RIGHT + 0.1))
+        # g = (1/(obstacle_UP + 0.1))
+        # h = (1/(obstacle_UP_RIGHT + 0.1))
+        # i = (1/(monster_UP + 0.1))
+        # j = (1/(monster_UP_RIGHT + 0.1))
+        # k = (1/(water_UP + 0.1))
+        # l = (1/(water_UP_RIGHT + 0.1))
 
-        f1 = (k/(oxygen + 0.1)) + l
-        f1 = self.normaliza_feature(f1, 110, 1.7355371901)
+        f1 = (obstacle_AHEAD + obstacle_DOWN + (obstacle_DOWN_RIGHT)*2)
+        f2 = ((monster_AHEAD)*2) + (monster_DOWN_RIGHT*2) + monster_UP_RIGHT
+        f3 = (obstacle_UP + obstacle_DOWN)*2
+        f4 = ((oxygen)*3) + ((water_UP)*2) + (water_UP_RIGHT * 2)
 
-        #max 30 min 2.7272727273    20.1    1.9181818182
-        f2 = (a + b + c)/(g + h + 0.1)
-        f2 = self.normaliza_feature(f2, 15.6398104264, 0.1356842103)
-
-        f3 = (d + f + e)/(i + j + 0.1)
-        f3 = self.normaliza_feature(f3, 15.6398104264, 0.1356842103)
+        f1 = self.normaliza_feature(f1, -1, 4)
+        f2 = self.normaliza_feature(f2, -1, 5)
+        f3 = self.normaliza_feature(f3, -1, 4)
+        f4 = self.normaliza_feature(f4, -1, 7)
 
         features_list.append(f1)
         features_list.append(f2)
         features_list.append(f3)
+        features_list.append(f4)
         return features_list
 
         """
@@ -102,65 +104,64 @@ class Controller(controller_template.Controller):
         12	oxygen: 1-400
 
     """
+            #gera vizinhos de um estado
+    def geraVizinhos(self,estado):
+        perturbacao = 0.5
+        vizinhos = []
 
-    #funcao de aprendizado
+
+        vi = estado[:]
+
+
+        for j in range(len(vi)):
+            perturbacao = random.uniform(-5,5)
+            vi[j] += perturbacao
+
+        return vi[:]
+
     def learn(self, weights):
-        #gera um estado inicial com os valores theta iniciais
-        def selecionaEstadoAleatorio(self, sensors):
-            estado = []
-            estado.append(0.5)
-            estado.append(0.5)
-            estado.append(0.5)
-            return estado
 
 
-        #gera vizinhos de um estado
-        def geraVizinhos(estado, numVizinhos):
-            vizinhos = []
-            for i in range(numVizinhos):
-	            new_weights = estado[:]
-	            disturbance = gera_ruido(new_weights)
-	            vizinhos.append(disturbance)
-            return vizinhos
+        estado_atual = self.geraVizinhos([1,1,1,1,1,1,1,1])
+        min_temperatura = 0
+        T = 400
+        while 1==1:
 
-        def gera_ruido(list):
-            list_size = len(list)
-            ruido = 0.
-            #for each value of the state
-            for i in range(list_size):
-                rand_choice = random.randint(0, 1)
-                if(rand_choice == 0):
-                    list[i] -= ruido #random.uniform(0, 10)
-                elif(rand_choice == 1):
-                    list[i] += ruido #random.uniform(0, 10)
-            return list
+            T = T - 0.5
+            print (T)
+            if( T == min_temperatura):
+                return estado_atual
 
-        #define estado inicial como um estado aleatorio
 
-        # estado_atual = [random.uniform(-1, 1), random.uniform(-1, 1),random.uniform(-1, 1),
-        # random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1),
-        # random.uniform(-1, 1)]
-        estado_atual = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
-        while (1 == 1):
-            v = geraVizinhos(estado_atual, 60)
-            melhor_vizinho = estado_atual[:]
-            for vizinhos in v:
-                #testa os vizinhos do estado atual, indo sempre para o melhor vizinho
-                print (melhor_vizinho)
-                melhor_atual = self.run_episode(melhor_vizinho)
-                vizinho_atual = self.run_episode(vizinhos)
-                print ("melhor vizinho atual com valor: ", melhor_atual)
-                print ("vizinho atual com valor: ", vizinho_atual)
-                if (vizinho_atual > melhor_atual):
-                    melhor_vizinho = vizinhos[:]
-            #depois de passar por todos vizinhos, verifica se o melhor vizinho eh melhor
-            #que o estado atual (inicial, definido como aleatorio)
-            if (self.run_episode(melhor_vizinho) > self.run_episode(estado_atual)):
-                print ("melhor vizinho final aqui ", melhor_vizinho)
-                estado_atual = melhor_vizinho[:]
-            else:
-                print ("melhor vizinho eh o original ", estado_atual)
-                pass
+            for i in range(1,30):
+                estado_candidato = self.geraVizinhos(estado_atual)
+                candidato = self.run_episode(estado_candidato)
+                atual = self.run_episode(estado_atual)
+                variacao_E =  candidato - atual
+
+                print("Variação de E :",variacao_E)
+                print("Valor do estado atual:", atual)
+                print("Valor do estado candidato:", candidato)
+                print("Estado Atual:", estado_atual)
+
+
+                if (variacao_E > 0):
+                    print("Variação positiva")
+                    estado_atual = estado_candidato[:]
+
+                elif(variacao_E < 0):
+                    value = math.exp(variacao_E/T)
+                    verifica_prob = random.uniform(0,1)
+
+                    print("variação negativa")
+                    print("verifica prob:", verifica_prob)
+                    print('Função de prob: %.10f' % value)
+
+                    if(verifica_prob < value):
+                        print("Acertou probabilidade")
+                        estado_atual = estado_candidato[:]
+
+                
 
         """
         HINT: you can call self.run_episode (see controller_template.py) to evaluate a given set of weights
